0dd00603f03739c8da25645c3f8ed2b21652b6b8
orynb <orynbassarovadinara2311@gmail.com>
2024-12-20 08:47:07 +0500

    init commit

---
+++ src/main/java/com/example/githubintelij/GithubIntelijApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubintelij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntelijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubIntelijApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubintelij/GithubIntelijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubintelij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntelijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
